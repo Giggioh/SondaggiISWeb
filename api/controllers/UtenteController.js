7a9c784187862c48477a65a05fc183f52209c1bb
Giggioh <33316356+Giggioh@users.noreply.github.com>
2018-05-10 21:18:08 +0200

    logging

--- api/controllers/UtenteController.js
+++ api/controllers/UtenteController.js
@@ -18,13 +18,16 @@ module.exports = {
     var nome=req.param("utente").nome;
     var cognome=req.param("utente").cognome;
     var data_nascita=req.param("utente").data_nascita;
+	console.log('1');
 
     sails.getDatastore().transaction(function(db,proceed) {
       Account.create({username: username, hash: hash}).usingConnection(db).exec(function(err,account) {
         if (err) return proceed(err);
+		console.log('2');
 
         Utente.create({nome: nome, cognome: cognome,data_nascita:new Date(data_nascita), account: account.id}).usingConnection(db).exec(function(err2,utente) {
           if (err2) return proceed(err2);
+			console.log('3');
 
           return proceed();
         });
